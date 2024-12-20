1115880e43027c524b98ad0ec8ff3a86f9f1f024
madhu1234madhu <84980502+madhu1234madhu@users.noreply.github.com>
2024-12-20 21:39:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-// app.js
+
 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config(); // Load .env file for sensitive data
-const userRoutes = require('./routes/userRoutes'); // Import user routes
-const transactionRoutes = require('./routes/transactionRoutes'); // Import transaction routes
+require('dotenv').config(); 
+const userRoutes = require('./routes/userRoutes'); 
+const transactionRoutes = require('./routes/transactionRoutes'); 
 const rateLimit = require('express-rate-limit');
 
-// Initialize Express app
+
 const app = express();
 
-// Middleware setup
-app.use(cors()); // Enable Cross-Origin Resource Sharing
-app.use(bodyParser.json()); // Parse JSON data from incoming requests
-app.use(bodyParser.urlencoded({ extended: true })); // Parse URL encoded data
 
-// MongoDB connection
+app.use(cors()); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -26,18 +25,18 @@ mongoose.connect(process.env.DB_URI, {
     .catch(err => console.log('Database connection failed:', err));
 
     const limiter = rateLimit({
-        windowMs: 60 * 60 * 1000, // 1 hour window
-        max: 100, // limit to 100 requests per windowMs
+        windowMs: 60 * 60 * 1000, 
+        max: 100, 
         message: 'Too many requests from this IP, please try again later.'
     });
     
     app.use(limiter);
 
-// Use Routes
-app.use('/api/users', userRoutes); // Register the user routes
-app.use('/api/transactions', transactionRoutes); // Register the transaction routes
 
-// Set the port for the server
+app.use('/api/users', userRoutes); 
+app.use('/api/transactions', transactionRoutes); 
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
